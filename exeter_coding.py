698b288ef9377e5cf0a22722b85cff1d8a3bb477
KrithikMurugesan <54681366+KrithikMurugesan@users.noreply.github.com>
2021-07-06 09:31:30 +0530

    Add files via upload

--- exeter_coding.py
+++ exeter_coding.py
@@ -1,10 +1,12 @@
+
+
 import csv
 
 import re
 
 d={}
 def csvToDictionary():
-    reader = csv.reader(open('french_dictionary.csv', 'r'))
+    reader = csv.reader(open('/content/drive/MyDrive/Exeter/french_dictionary.csv', 'r'))
     d = {}
     for row in reader:
        k, v = row
@@ -14,16 +16,15 @@ def csvToDictionary():
     
 
 def textToList():
-    my_file = open("find_words.txt", "r")
+    my_file = open("/content/drive/MyDrive/Exeter/find_words.txt", "r")
     content = my_file.read()
     content_list = content.split("\n")
-
     my_file.close()
     return content_list
 
 
 def inputfile():
-    my_file = open("t8.shakespeare.txt", "r")
+    my_file = open("/content/drive/MyDrive/Exeter/t8.shakespeare.txt", "r")
     content = my_file.read()
     my_file.close()
     return content    
@@ -40,15 +41,35 @@ if __name__ == "__main__":
         for word in text:    
             entry=[]
             n=inputFile.count(word)
+            entry.append(word)
+            entry.append(csvDict[word])
+            entry.append(n)
+            writer.writerow(entry)
             if(n>0):
-                entry.append(word)
-                entry.append(csvDict[word])
-                entry.append(n)
-                writer.writerow(entry)
-                inputFile=re.sub((word.lower()),( csvDict[word].lower()),inputFile)
-                inputFile=re.sub((word.upper()), (csvDict[word].upper()),inputFile)
-                inputFile=re.sub((word.capitalize()), (csvDict[word].capitalize()),inputFile)
-
+                obj=re.compile((word.lower()))
+                inputFile=obj.sub((csvDict[word].lower()),inputFile)
+                #inputFile=re.sub((word.lower()),(csvDict[word].lower()),inputFile)
+                #inputFile=inputFile.replace((word.lower()),(csvDict[word].lower()))
+            n=inputFile.count(word)
+            if(n>0):
+                obj=re.compile((word.capitalize()))
+                inputFile=obj.sub((csvDict[word].capitalize()),inputFile)
+                #inputFile=re.sub((word.capitalize()), (csvDict[word].capitalize()),inputFile)
+                #inputFile=inputFile.replace((word.capitalize()),(csvDict[word].capitalize()))
                 
+            n=inputFile.count(word)
+            if(n>0):
+                obj=re.compile((word.upper()))
+                inputFile=obj.sub((csvDict[word].upper()),inputFile)
+                #inputFile=re.sub((word.upper()),( csvDict[word].upper()),inputFile)
+                #inputFile=inputFile.replace((word.upper()),(csvDict[word].upper()))
+
+            n=inputFile.count(word)
+            if(n>0):
+                obj=re.compile(word,flags=re.IGNORECASE)
+                inputFile=obj.sub((csvDict[word]),inputFile)
+                #inputFile=re.sub(word,csvDict[word],inputFile,flags=re.IGNORECASE)
+
+
     resultFile = open("t8.shakespeare.translated.txt", "w")
     resultFile.write(inputFile)
